dbd3de162ff5233369aac90243a869b9911979c2
Wesley Jacobs <wesleyjacobs@Wesleys-MacBook-Pro.local>
2019-09-27 13:01:02 -0700

    added by breed and ignore file

---
+++ byBreed/index.js
@@ -0,0 +1,34 @@
+// Function to fetch information from the API
+
+function getDogImages(value) {
+  let endPoint = 'https://dog.ceo/api/breeds/image/random/';
+  console.log(endPoint + value);
+  fetch(endPoint + value)
+    .then(response => response.json())
+    .then(responseJson => displayResults(responseJson))
+    .catch(error => console.log('sorry this did not work'));
+  //console.log(getDogImages);
+}
+
+//handle function for the submit
+function handleSubmitButton() {
+  $('form').submit(event => {
+    event.preventDefault();
+    let value = parseInt($('#value').val());
+    console.log(typeof value);
+    getDogImages(value);
+    // console.log(handleSubmitButton);
+  });
+}
+
+function displayResults(responseJson) {
+  for( let i = 0; i < responseJson.message.length; i++){
+    console.log(responseJson.message[i]);
+    $('.results').append(`<img src='${responseJson.message[i]}' id="Img-holder">`);
+  }
+}
+
+$(function() {
+  console.log('app loaded! waiting for submit!');
+  handleSubmitButton();
+});
